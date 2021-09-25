bd11a3f6bd454475261645fc38c3b4e48f27557c
Nirmal-C <chathuraun@gmail.com>
2021-09-25 11:15:41 +0530

    Feat: Initialized Gallery

---
+++ lib/utils/imagePreferences.dart
@@ -0,0 +1,124 @@
+import 'package:backover/model/images.dart';
+
+List<ImageDetails> images = [
+  ImageDetails(
+    imagePath: 'assets/1.jpg',
+    price: 'Price',
+    photographer: 'Photograher',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/2.jpg',
+    price: 'Price',
+    photographer: 'Photographer',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/3.jpg',
+    price: 'Price',
+    photographer: 'Photographer',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/1.jpg',
+    price: 'Price',
+    photographer: 'Photograher',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/2.jpg',
+    price: 'Price',
+    photographer: 'Photographer',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/3.jpg',
+    price: 'Price',
+    photographer: 'Photographer',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/1.jpg',
+    price: 'Price',
+    photographer: 'Photograher',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/2.jpg',
+    price: 'Price',
+    photographer: 'Photographer',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/3.jpg',
+    price: 'Price',
+    photographer: 'Photographer',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/1.jpg',
+    price: 'Price',
+    photographer: 'Photograher',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/2.jpg',
+    price: 'Price',
+    photographer: 'Photographer',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/3.jpg',
+    price: 'Price',
+    photographer: 'Photographer',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/1.jpg',
+    price: 'Price',
+    photographer: 'Photograher',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/2.jpg',
+    price: 'Price',
+    photographer: 'Photographer',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+  ImageDetails(
+    imagePath: 'assets/3.jpg',
+    price: 'Price',
+    photographer: 'Photographer',
+    title: 'Title',
+    details:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil error aspernatur, sequi inventore eligendi vitae dolorem animi suscipit. Nobis, cumque.',
+  ),
+];
