c5113008fdcfe52e3a3a9f1962f7b6b95d461c3a
Nirmal-C <chathuraun@gmail.com>
2021-09-26 09:09:00 +0530

    UI: Onboard Screen Modified

--- lib/model/onboardingScreen.dart
+++ lib/model/onboardingScreen.dart
@@ -10,7 +10,7 @@ List<OnboardingModel> onboardingContent = [
   OnboardingModel(
       title: 'Welcome to A',
       image:
-          'https://res.cloudinary.com/dxmxhyzsy/image/upload/v1632625953/onboard2_jfbnid.png',
+          'https://res.cloudinary.com/dguy8qpzi/image/upload/v1632546428/backover-1_qbzqax.png',
       descp: "Let's make over your backyard with the power of community"),
   OnboardingModel(
       title: 'Welcome to B',
@@ -20,6 +20,6 @@ List<OnboardingModel> onboardingContent = [
   OnboardingModel(
       title: 'Welcome to C',
       image:
-          'https://res.cloudinary.com/dguy8qpzi/image/upload/v1632546428/backover-1_qbzqax.png',
+          'https://res.cloudinary.com/dxmxhyzsy/image/upload/v1632625953/onboard2_jfbnid.png',
       descp: "Let's make over your backyard with the power of community"),
 ];
